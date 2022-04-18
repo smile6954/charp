1492b5d9ca61c19f0491055fc1c9e8e781dd9622
Leonid Soloviev <solovievleon@gmail.com>
2022-04-18 20:13:07 +0300

    hello example

---
+++ exmpl2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username == "Masha")
+{
+    Console.WriteLine("Hello, Masha, i am glad to see you");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
